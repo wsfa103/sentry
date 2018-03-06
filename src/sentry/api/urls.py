ad00d4e6f3bf3a4730b0d1379a926734172aec35
Armin Ronacher <armin.ronacher@active-4.com>
2018-03-06 01:48:32 +0100

    fix: Some small fixes on the relay endpoints

--- src/sentry/api/urls.py
+++ src/sentry/api/urls.py
@@ -190,7 +190,7 @@ urlpatterns = patterns(
     ),
 
     url(
-        r'^relay/aorta/$',
+        r'^relays/heartbeat/$',
         RelayAortaEndpoint.as_view(),
         name='sentry-api-0-relay-aorta'
     ),

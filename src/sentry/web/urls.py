ce3fed088f51de3c842aeea4e4945405efb2e4a8
Lauryn Brown <lauryndbrown@gmail.com>
2018-04-18 18:37:34 -0700

    first draft of vsts integration

--- src/sentry/web/urls.py
+++ src/sentry/web/urls.py
@@ -484,6 +484,7 @@ urlpatterns += patterns(
         IntegrationSetupView.as_view()),
     url(r'^extensions/cloudflare/', include('sentry.integrations.cloudflare.urls')),
     url(r'^extensions/slack/', include('sentry.integrations.slack.urls')),
+    url(r'^extensions/vsts/', include('sentry.integrations.vsts.urls')),
 
     url(r'^plugins/', include('sentry.plugins.base.urls')),
 
